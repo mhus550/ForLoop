d8944b386b024d151c6022c2c8f52ae879e06189
Mohammad H <mhus3040@gmail.com>
2022-12-29 07:04:42 -0500

    working on for loop class

---
+++ forLoop.java
@@ -0,0 +1,23 @@
+package Review;
+
+
+public class ForLoop {
+	public static void main(String[] args) {
+		
+		// for loop: It provides a concise way of writing the loop structure.
+		// The "for statement" consumes the initialization, condition and increment/decrement in one line
+		// thereby providing a shorter, easy to debug structure of looping.
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");  // 1 2 3 4 5 6 7 8 9 10 
+		}
+		
+		int sum = 0;
+		
+		for (int x = 1; x <= 10; x++) {
+			sum = sum + x;	
+		}
+		System.out.println("The sum: " + sum); //  The sum: 55
+		
+	}
+}
